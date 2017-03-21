c7e5b8c3171f0016418e1990eb16ac17e1c61d4f
Hunter Chang <hunter@madwiremedia.com>
2017-03-21 11:37:59 -0600

    add clock and timer

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
+var Clock = require('./Clock');
 
 var Timer = React.createClass({
     render: function() {
         return (
-            <p>Timer.jsx</p>
+            <div>
+                <Clock />
+            </div>
         )
     }
 });
